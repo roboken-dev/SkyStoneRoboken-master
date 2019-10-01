c7b9cfb0786980ca94f16da37f071a809fee2a87
Chris Kunkel <ckunkel@hobokencs.net>
2019-10-01 15:59:30 -0400

    added telemetry to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -21,6 +21,9 @@ public class Teleop extends LinearOpMode {
         robot.motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
         robot.motorRearLeft.setDirection(DcMotor.Direction.REVERSE);
 
+        telemetry.addData("Status", "Ready to Go");    //
+        telemetry.update();
+
         waitForStart();
 
         while (opModeIsActive()) {
@@ -30,15 +33,23 @@ public class Teleop extends LinearOpMode {
             float G1leftTrigger = gamepad1.left_trigger;
             if (gamepad1.dpad_up) {
                 speed_control = 1;
+                telemetry.addData("Status", "Setting Speed to 1");    //
+                telemetry.update();
             }
             if (gamepad1.dpad_down) {
                 speed_control = 0.25f;
+                telemetry.addData("Status", "Setting Speed to .25");    //
+                telemetry.update();
             }
             if (gamepad1.dpad_left) {
                 speed_control = 0.5f;
+                telemetry.addData("Status", "Setting Speed to .5");    //
+                telemetry.update();
             }
             if (gamepad1.dpad_right) {
                 speed_control = 0.5f;
+                telemetry.addData("Status", "Setting Speed to .5");    //
+                telemetry.update();
             }
 
             if (G1rightTrigger > 0 && G1leftTrigger == 0) {
@@ -47,11 +58,19 @@ public class Teleop extends LinearOpMode {
                 robot.motorRearLeft.setPower(G1rightTrigger * speed_control);
                 robot.motorFrontRight.setPower(G1rightTrigger * speed_control);
                 robot.motorRearRight.setPower(-G1rightTrigger * speed_control);
+
+                telemetry.addData("Status", "Strafing Right");    //
+                telemetry.update();
+
             } else if (G1leftTrigger > 0 && G1rightTrigger == 0) {
                 robot.motorFrontLeft.setPower(G1leftTrigger * speed_control);
                 robot.motorRearLeft.setPower(-G1leftTrigger * speed_control);
                 robot.motorFrontRight.setPower(-G1leftTrigger * speed_control);
                 robot.motorRearRight.setPower(G1leftTrigger * speed_control);
+
+                telemetry.addData("Status", "Strafing Left");    //
+                telemetry.update();
+
             } else {
 
                 // how to cube  x = Math.pow(y, 3);
@@ -59,13 +78,22 @@ public class Teleop extends LinearOpMode {
                 robot.motorRearLeft.setPower(G1leftStickY * speed_control);
                 robot.motorFrontRight.setPower(G1rightStickY * speed_control);
                 robot.motorRearRight.setPower(G1rightStickY * speed_control);
+
+                telemetry.addData("Status", "Moving");    //
+                telemetry.update();
+
             }
             if (gamepad1.left_bumper) {
                 robot.Claw.setPosition(0.0);
+                telemetry.addData("Status", "Claw");    //
+                telemetry.update();
+
             }
 
             if (gamepad1.right_bumper) {
                 robot.Claw.setPosition(1.0);
+                telemetry.addData("Status", "Claw");    //
+                telemetry.update();
                 idle();
             }
         }

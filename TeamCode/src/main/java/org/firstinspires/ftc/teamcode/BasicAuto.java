aa3abefce9ddec70f97a41ac2edf4a01f0ccb312
Chris Kunkel <ckunkel@hobokencs.net>
2019-10-01 15:44:14 -0400

    initial encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto.java
@@ -9,51 +9,141 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Autonomous (name = "BasicAuto")
 
 public class BasicAuto extends LinearOpMode {
 
-    DcMotor motorFrontLeft;  // motor1
-    DcMotor motorRearLeft;  // motor 2
-    DcMotor motorFrontRight; // motor 3
-    DcMotor motorRearRight; // motor 4
-    private Servo Claw;
+    Robokenbot robot   = new Robokenbot();   // Use a Pushbot's hardware
+    private ElapsedTime     runtime = new ElapsedTime();
 
-    DigitalChannel digitalTouch;  // Hardware Device Object
-    ColorSensor sensorColor;
-    DistanceSensor sensorDistance;
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 0.5 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
 
     double DRIVE_POWER = 1;
 
     public void runOpMode() throws InterruptedException {
+        /*
+         * Initialize the drive system variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
 
-        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
-        motorRearLeft = hardwareMap.dcMotor.get("motorRearLeft");
-        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        motorRearRight = hardwareMap.dcMotor.get("motorRearRight");
- //       digitalTouch = hardwareMap.get(DigitalChannel.class, "sensor_digital");
-        Claw = hardwareMap.servo.get("servo");
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Resetting Encoders");    //
+        telemetry.update();
+
+        robot.motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Path0",  "Starting at %7d :%7d",
+                robot.motorFrontLeft.getCurrentPosition(),
+                robot.motorFrontRight.getCurrentPosition());
+        telemetry.update();
+
 
         waitForStart();
+        // Step through each leg of the path,
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
+
+        sleep(1000);     // pause for servos to move
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        /*
         StrafeRightTime(DRIVE_POWER, 4000);
 
         DriveForwardTime(DRIVE_POWER, 2000);
 
         // lower and grab foundation = need real claw cdde
-        Claw.setPosition(0.0);
+        robot.Claw.setPosition(0.0);
         sleep(500);
-        Claw.setPosition(1.0);
+        robot.Claw.setPosition(1.0);
         DriveForwardTime(-DRIVE_POWER, 2000);
         StopDriving();
+
+         */
+    }
+
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = robot.motorFrontLeft.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = robot.motorFrontRight.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            robot.motorFrontLeft.setTargetPosition(newLeftTarget);
+            robot.motorFrontRight.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.motorFrontLeft.setPower(Math.abs(speed));
+            robot.motorFrontRight.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.motorFrontLeft.isBusy() && robot.motorFrontRight.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        robot.motorFrontLeft.getCurrentPosition(),
+                        robot.motorFrontRight.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            robot.motorFrontLeft.setPower(0);
+            robot.motorFrontRight.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            robot.motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        }
     }
 
     public void DriveForward(double power) {
-        motorFrontLeft.setPower(power);
-        motorFrontRight.setPower(power);
+        robot.motorFrontLeft.setPower(power);
+       robot.motorFrontRight.setPower(power);
     }
 
     public void StopDriving() {
@@ -61,8 +151,8 @@ public class BasicAuto extends LinearOpMode {
     }
 
     public void TurnLeft(double power) {
-        motorFrontLeft.setPower(-power);
-        motorFrontRight.setPower(power);
+        robot.motorFrontLeft.setPower(-power);
+        robot.motorFrontRight.setPower(power);
     }
 
     public void TurnRight(double power) {
@@ -86,7 +176,7 @@ public class BasicAuto extends LinearOpMode {
 
 
     public void DriveTillTouch(double power) {
-        while (digitalTouch.getState() == true) {
+        while (robot.digitalTouch.getState() == true) {
             DriveForward(power);
         }
 
@@ -95,7 +185,7 @@ public class BasicAuto extends LinearOpMode {
     public void DriveTillDistance(double power, double distance) {
         double howfar;
 
-        while (Double.isNaN(sensorDistance.getDistance(DistanceUnit.CM)) || sensorDistance.getDistance(DistanceUnit.CM) > distance) {
+        while (Double.isNaN(robot.sensorDistance.getDistance(DistanceUnit.CM)) || robot.sensorDistance.getDistance(DistanceUnit.CM) > distance) {
             DriveForward(power);
         }
 
@@ -114,17 +204,17 @@ public class BasicAuto extends LinearOpMode {
 
     public void StrafeRight(double power)
     {
-        motorFrontLeft.setPower(-power);
-        motorRearLeft.setPower(power);
-        motorFrontRight.setPower(power);
-        motorRearRight.setPower(-power);
+        robot.motorFrontLeft.setPower(-power);
+        robot.motorRearLeft.setPower(power);
+        robot.motorFrontRight.setPower(power);
+        robot.motorRearRight.setPower(-power);
     }
 
     public void StrafeLeft(double power) {
-        motorFrontLeft.setPower(power);
-        motorRearLeft.setPower(-power);
-        motorFrontRight.setPower(-power);
-        motorRearRight.setPower(power);
+        robot.motorFrontLeft.setPower(power);
+        robot.motorRearLeft.setPower(-power);
+        robot.motorFrontRight.setPower(-power);
+        robot.motorRearRight.setPower(power);
     }
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robokenbot.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class Robokenbot
+{
+    public DcMotor motorFrontLeft;  // motor1
+    public DcMotor motorRearLeft;  // motor 2
+    public DcMotor motorFrontRight; // motor 3
+    public DcMotor motorRearRight; // motor 4
+    public Servo Claw;
+
+    public DigitalChannel digitalTouch;  // Hardware Device Object
+    public ColorSensor sensorColor;
+    public DistanceSensor sensorDistance;
+
+HardwareMap hwMap = null;
+private ElapsedTime period =new ElapsedTime();
+
+public Robokenbot(){
+
+
+
+}
+
+public void init(HardwareMap ahwMap) {
+
+    hwMap=ahwMap;
+    motorFrontLeft = hwMap.dcMotor.get("motorFrontLeft");
+    motorRearLeft = hwMap.dcMotor.get("motorRearLeft");
+    motorFrontRight = hwMap.dcMotor.get("motorFrontRight");
+    motorRearRight = hwMap.dcMotor.get("motorRearRight");
+    //       digitalTouch = hardwareMap.get(DigitalChannel.class, "sensor_digital");
+    Claw = hwMap.servo.get("servo");
+
+}
+
+
+
+
+
+
+
+}
